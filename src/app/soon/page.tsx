6a0b22600e1167c53364f42bae9ed14f90b8252b
Berke Kanber <berkekanber@gmail.com>
2024-09-14 19:15:11 +0300

    refactor: soon pages.

--- src/app/soon/page.tsx
+++ src/app/soon/page.tsx
@@ -6,8 +6,8 @@ import logo from "../../../public/testlogo.svg";
 function page() {
   return (
     <>
-      <div className="w-64 h-64 bg-pink-500 blur-[250px] absolute top-1/4 left-3/4 z-0"></div>
-      <div className="w-64 h-64 bg-pink-500 blur-[250px] absolute top-1/2 right-3/4 z-0"></div>
+      <div className="w-64 h-64 bg-pink-500 blur-[200px] absolute top-1/4 left-3/4 z-0"></div>
+      <div className="w-64 h-64 bg-pink-500 blur-[200px] absolute top-1/2 right-3/4 z-0"></div>
       <div className="flex flex-col items-center justify-center w-full h-screen relative z-10 pt-72">
         <Image src={logo} width={130} height={130} alt="logo" />
         <div className="font-black text-[64px] pt-8 text-pink-500">Cooking Our Website.</div>
