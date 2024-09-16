e420405c17abfe4f939854349ab04ad6a21cdfc7
Harun <spadeycontent@gmail.com>
2024-09-17 01:33:12 +0300

    feat: redesign contributos

--- src/components/pages/OverviewContent.tsx
+++ src/components/pages/OverviewContent.tsx
@@ -7,6 +7,7 @@ import { AppDispatch } from "@/lib/store";
 import GithubCalendar from "@/components/dashboard/GithubCalendar";
 import { fetchUser } from "@/lib/features/userSlice";
 import { fetchUserEvents } from "@/lib/features/userEventsSlice";
+import { LuCalendarDays } from "react-icons/lu";
 
 import Tooltips from "@/components/dashboard/Tooltip";
 
@@ -70,13 +71,17 @@ function OverviewContent() {
         : null;
     const percentageHourspent = (commitsRate?.length + prRate?.length) / 3;
 
-    const percentageCommits = (commitsRate?.length / events?.userEvent?.length) * 100;
-    const percentageIssues = (issuesRate?.length / events?.userEvent?.length) * 100;
-    const percentagePullRequest = (prRate?.length / events?.userEvent?.length) * 100;
+    const percentageCommits =
+      (commitsRate?.length / events?.userEvent?.length) * 100;
+    const percentageIssues =
+      (issuesRate?.length / events?.userEvent?.length) * 100;
+    const percentagePullRequest =
+      (prRate?.length / events?.userEvent?.length) * 100;
 
     const averageHourSpent = (commitsRate?.length + prRate?.length) * 0.5;
 
-    const averageEventTotal = commitsRate?.length + issuesRate?.length + prRate?.length;
+    const averageEventTotal =
+      commitsRate?.length + issuesRate?.length + prRate?.length;
     const percentageEventTotal = averageEventTotal / 30;
 
     setAverageRate({
@@ -109,14 +114,26 @@ function OverviewContent() {
 
   return (
     <div className="m-8">
-      <h2 className="text-lg font-semibold text-gray-800 flex-1 mb-6">Overview</h2>
+      <h2 className="text-lg font-semibold text-gray-800 flex-1 mb-6">
+        Overview
+      </h2>
       <div className="w-full h-auto text-gray-800 rounded-lg p-6 space-y-6">
         <div className="flex justify-between">
           <div className="flex items-center space-x-6">
-            <img src={state.userProfile.avatar_url} width={120} height={120} alt="userimage" className="border border-gray-200 rounded-full object-cover" />
+            <img
+              src={state.userProfile.avatar_url}
+              width={120}
+              height={120}
+              alt="userimage"
+              className="border border-gray-200 rounded-full object-cover"
+            />
             <div>
-              <p className="text-2xl font-semibold text-gray-800">@{state.userProfile.login}</p>
-              <p className="text-gray-600 text-sm">ID: {state.userProfile.id}</p>
+              <p className="text-2xl font-semibold text-gray-800">
+                @{state.userProfile.login}
+              </p>
+              <p className="text-gray-600 text-sm">
+                ID: {state.userProfile.id}
+              </p>
               <p className="text-md text-gray-700">{state.userProfile.bio}</p>
             </div>
           </div>
@@ -126,14 +143,17 @@ function OverviewContent() {
           <div className="flex-col items-center">
             <div className="flex items-center gap-5">
               <div>
-                {state.userProfile.followers} <span className="text-gray-500">Followers</span>
+                {state.userProfile.followers}{" "}
+                <span className="text-gray-500">Followers</span>
               </div>
               <div>
-                {state.userProfile.following} <span className="text-gray-500">Followers</span>
+                {state.userProfile.following}{" "}
+                <span className="text-gray-500">Followers</span>
               </div>
             </div>
             <div className="flex items-center justify-start gap-2 pt-4">
-              <FaMapMarkerAlt className="text-gray-700" /> {state.userProfile.location}
+              <FaMapMarkerAlt className="text-gray-700" />{" "}
+              {state.userProfile.location}
             </div>
             <div className="flex items-center justify-start gap-2 pt-1">
               <FaCodeBranch />
@@ -154,11 +174,29 @@ function OverviewContent() {
             <div className="flex items-center justify-center gap-8 pt-4">
               <div>
                 <p className="text-gray-400 text-sm">Your Average</p>
-                <p className="font-medium text-lg">{averages.commits.average}</p>
+                <p className="font-medium text-lg">
+                  {averages.commits.average}
+                </p>
               </div>
-              <div className={`${averages.commits.percentage > 30 ? "bg-green-400/40" : "bg-red-400/40"} rounded-lg px-2 py-1.5`}>
-                <h2 className={`${averages.commits.percentage > 30 ? "text-green-600" : "text-red-600"}`}>
-                  {averages.commits.percentage > 30 ? <p>+{averages.commits.percentage.toFixed(1)}%</p> : <p>-{averages.commits.percentage.toFixed(1)}%</p>}
+              <div
+                className={`${
+                  averages.commits.percentage > 30
+                    ? "bg-green-400/40"
+                    : "bg-red-400/40"
+                } rounded-lg px-2 py-1.5`}
+              >
+                <h2
+                  className={`${
+                    averages.commits.percentage > 30
+                      ? "text-green-600"
+                      : "text-red-600"
+                  }`}
+                >
+                  {averages.commits.percentage > 30 ? (
+                    <p>+{averages.commits.percentage.toFixed(1)}%</p>
+                  ) : (
+                    <p>-{averages.commits.percentage.toFixed(1)}%</p>
+                  )}
                 </h2>
               </div>
             </div>
@@ -170,9 +208,25 @@ function OverviewContent() {
                 <p className="text-gray-400 text-sm">Your Average</p>
                 <p className="font-medium text-lg">{averages.issues.average}</p>
               </div>
-              <div className={`${averages.issues.percentage > 30 ? "bg-green-400/40" : "bg-red-400/40"} rounded-lg px-2 py-1.5`}>
-                <h2 className={`${averages.issues.percentage > 30 ? "text-green-600" : "text-red-600"}`}>
-                  {averages.issues.percentage > 30 ? <p>+{averages.issues.percentage.toFixed(1)}%</p> : <p>-{averages.issues.percentage.toFixed(1)}%</p>}
+              <div
+                className={`${
+                  averages.issues.percentage > 30
+                    ? "bg-green-400/40"
+                    : "bg-red-400/40"
+                } rounded-lg px-2 py-1.5`}
+              >
+                <h2
+                  className={`${
+                    averages.issues.percentage > 30
+                      ? "text-green-600"
+                      : "text-red-600"
+                  }`}
+                >
+                  {averages.issues.percentage > 30 ? (
+                    <p>+{averages.issues.percentage.toFixed(1)}%</p>
+                  ) : (
+                    <p>-{averages.issues.percentage.toFixed(1)}%</p>
+                  )}
                 </h2>
               </div>
             </div>
@@ -184,25 +238,62 @@ function OverviewContent() {
                 <p className="text-gray-400 text-sm">Your Average</p>
                 <p className="font-medium text-lg">{averages.pr.average}</p>
               </div>
-              <div className={`${averages.pr.percentage > 30 ? "bg-green-400/40" : "bg-red-400/40"} rounded-lg px-2 py-1.5`}>
-                <h2 className={`${averages.pr.percentage > 30 ? "text-green-600" : "text-red-600"}`}>
-                  {averages.pr.percentage > 30 ? <p>+{averages.pr.percentage.toFixed(1)}%</p> : <p>-{averages.pr.percentage.toFixed(1)}%</p>}
+              <div
+                className={`${
+                  averages.pr.percentage > 30
+                    ? "bg-green-400/40"
+                    : "bg-red-400/40"
+                } rounded-lg px-2 py-1.5`}
+              >
+                <h2
+                  className={`${
+                    averages.pr.percentage > 30
+                      ? "text-green-600"
+                      : "text-red-600"
+                  }`}
+                >
+                  {averages.pr.percentage > 30 ? (
+                    <p>+{averages.pr.percentage.toFixed(1)}%</p>
+                  ) : (
+                    <p>-{averages.pr.percentage.toFixed(1)}%</p>
+                  )}
                 </h2>
               </div>
             </div>
           </div>
           <div>
             <h2 className="text-lg font-medium">
-              Time Spent <Tooltips text={"Time estimates are from commit history and may not be exact."} />
+              Time Spent{" "}
+              <Tooltips
+                text={
+                  "Time estimates are from commit history and may not be exact."
+                }
+              />
             </h2>
             <div className="flex items-center justify-center gap-8 pt-4">
               <div>
                 <p className="text-gray-400 text-sm">Your Average</p>
                 <p className="font-medium text-lg">{averages.hp.hour}/hr</p>
               </div>
-              <div className={`${averages.hp.percentage < averages.hp.hour ? "bg-green-400/40" : "bg-red-400/40"} rounded-lg px-2 py-1.5`}>
-                <h2 className={`${averages.hp.percentage < averages.hp.hour ? "text-green-600" : "text-red-600"}`}>
-                  {averages.hp.percentage < averages.hp.hour ? <p>+{averages.hp.percentage.toFixed(1)}%</p> : <p>-{averages.hp.percentage.toFixed(1)}%</p>}
+              <div
+                className={`${
+                  averages.hp.percentage < averages.hp.hour
+                    ? "bg-green-400/40"
+                    : "bg-red-400/40"
+                } rounded-lg px-2 py-1.5`}
+              >
+                <h2
+                  className={`${
+                    averages.hp.percentage < averages.hp.hour
+                      ? "text-green-600"
+                      : "text-red-600"
+                  }`}
+                >
+                  {averages.hp.percentage < averages.hp.hour ? (
+                    <p>+{averages.hp.percentage.toFixed(1)}%</p>
+                  ) : (
+                    <p>-{averages.hp.percentage.toFixed(1)}%</p>
+                  )}
                 </h2>
               </div>
             </div>
@@ -210,20 +301,51 @@ function OverviewContent() {
         </div>
       </div>
 
-      <div className="mt-10 border-t pt-6 border-gray-200">
+      <div className="mt-10 border-t pt-6 border-gray-100 w-full">
         <h2 className="text-lg font-medium mb-8">Contributions</h2>
         <div className="flex items-center justify-between">
           <GithubCalendar />
-          <div className="bg-gray-100 px-11 py-2 rounded-lg flex flex-col items-center justify-center">
-            <h2 className="text-lg font-medium pt-2">Monthly</h2>
+          <div className="bg-gray-100 px-16 py-2 rounded-lg flex flex-col items-center justify-center">
+            <div className="relative flex justify-center items-center">
+              <h2 className="text-lg font-medium pt-2 flex justfiy-center items-center gap-2">
+                <LuCalendarDays />
+                Monthly
+              </h2>
+              <div className="absolute right-0 -top-1 transform translate-x-full translate-y-1 text-xl">
+                <Tooltips
+                  text={
+                    "Your total contributions were used to calculate your daily average by dividing them by monthly."
+                  }
+                />
+              </div>
+            </div>
+
             <div className="flex flex-col items-center justify-center gap-4 pt-2">
               <div className="flex-col items-center justify-center text-center">
                 <p className="text-gray-400 text-sm">Your Average</p>
-                <p className="font-medium text-lg">{averages.total.percentage.toFixed(2)}</p>
+                <p className="font-medium text-lg">
+                  {averages.total.percentage.toFixed(2)}
+                </p>
               </div>
-              <div className={`${averages.total.percentage < 0 ? "bg-green-400/40" : "bg-red-400/40"} rounded-lg px-2 py-1.5`}>
-                <h2 className={`${averages.total.percentage < 0 ? "text-green-600" : "text-red-600"}`}>
-                  {averages.total.percentage < 0 ? <p>+{averages.total.event.toFixed(2)}%</p> : <p>-{averages.total.event.toFixed(2)}%</p>}
+              <div
+                className={`${
+                  averages.total.percentage < 0
+                    ? "bg-green-400/40"
+                    : "bg-red-400/40"
+                } rounded-lg px-2 py-1.5`}
+              >
+                <h2
+                  className={`${
+                    averages.total.percentage < 0
+                      ? "text-green-600"
+                      : "text-red-600"
+                  }`}
+                >
+                  {averages.total.percentage < 0 ? (
+                    <p>+{averages.total.event.toFixed(2)}%</p>
+                  ) : (
+                    <p>-{averages.total.event.toFixed(2)}%</p>
+                  )}
                 </h2>
               </div>
             </div>
