4c98df98abac89d58f6bae51d35f1e6b22b47986
Berke Kanber <berkekanber@gmail.com>
2024-09-20 15:45:37 +0300

    feat: repository page filter feature

--- src/components/pages/RepositoryContent.tsx
+++ src/components/pages/RepositoryContent.tsx
@@ -4,7 +4,6 @@ import { fetchRepository } from "@/lib/features/repositorySlice";
 import { AppDispatch } from "@/lib/store";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { IoMdRefresh } from "react-icons/io";
 
 import { GrBook } from "react-icons/gr";
 import { FaCodeBranch } from "react-icons/fa";
@@ -24,41 +23,74 @@ function RepositoryContent() {
     dispatch(fetchRepository());
   }, [refresh]);
 
-  function handleRefresh() {
-    setRefresh(!refresh);
-  }
-
   function handleRepoRouter(repoId: any) {
     router.push(`/dashboard?repo=${repoId}`);
   }
 
-  /* const formatDate = (dateString: string) => {
-    const date = new Date(dateString);
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const year = date.getFullYear();
-    return `${day}.${month}.${year}`;
-  }; */
+  const [selectedOption, setSelectedOption] = useState("");
+  const [filteredData, setFilteredData] = useState<any[]>([]);
+
+  useEffect(() => {
+    setFilteredData(state.repository);
+  }, [state.repository]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    setSelectedOption(value);
+    handleFilteredData(value);
+  };
+
+  function handleFilteredData(option: string) {
+    if (option === "") {
+      return;
+    }
+
+    if (option === "star") {
+      const sortedDataForStars = [...state.repository].sort(
+        (a, b) => b.stargazers_count - a.stargazers_count
+      );
+      setFilteredData(sortedDataForStars);
+    }
+
+    if (option === "fork") {
+      const sortedDataForForks = [...state.repository].sort(
+        (a, b) => b.forks - a.forks
+      );
+      setFilteredData(sortedDataForForks);
+    }
+  }
 
   return (
     <div className="ml-8 mt-8 h-screen">
       <div className="flex-col h-full pb-24 ">
-        <div className="w-full flex items-center mb-6">
+        <div className="w-full flex items-center justify-between mb-6">
           <h2 className="text-lg font-semibold text-gray-800 flex-1">
             Repository{" "}
             <span className="text-gray-500 text-sm">
               ({state.repository.length || 0})
             </span>
           </h2>
-          <IoMdRefresh
-            onClick={handleRefresh}
-            className="text-gray-600 font-medium text-2xl cursor-pointer hover:text-gray-800 transition ml-auto"
-          />
+
+          <div className="flex items-center gap-3 mr-8">
+            <label className="text-md font-medium">Filter</label>
+            <select
+              id="filter"
+              value={selectedOption}
+              onChange={handleChange}
+              className="border border-gray-300 rounded-md py-1 px-2 focus:outline-none focus:ring focus:ring-pink-400 text-sm"
+            >
+              <option value="" disabled>
+                Select an option
+              </option>
+              <option value="star">Most stars</option>
+              <option value="fork">Most forks</option>
+            </select>
+          </div>
         </div>
 
-        <div className="grid grid-cols-2 mr-8 pb-8 max-md:grid-cols-1 gap-5">
-          {state.repository && state.repository.length > 0 ? (
-            state.repository.map((item: any, index: any) => {
+        <div className="grid grid-cols-2 mr-8 pb-8 max-md:grid-cols-1 gap-6">
+          {filteredData && filteredData.length > 0 ? (
+            filteredData.map((item: any, index: any) => {
               const updatedAt = new Date(item.updated_at);
               const formattedDate = `${updatedAt.getDate()}.${
                 updatedAt.getMonth() + 1
@@ -68,7 +100,7 @@ function RepositoryContent() {
                 <div
                   onClick={() => handleRepoRouter(item.id)}
                   key={`repo-${index}`}
-                  className="w-full h-48 bg-gray-200/20 mt-4 rounded-xl pl-6 pr-6 pt-6 border border-gray-200 hover:scale-105 transition-all hover:cursor-pointer"
+                  className="w-full h-52 bg-gray-200/20 mt-4 rounded-xl pl-6 pr-6 pt-6 border border-gray-200 hover:scale-105 transition-all hover:cursor-pointer"
                 >
                   <div className="flex items-center w-full justify-between">
                     <div className="flex items-center gap-2 underline">
